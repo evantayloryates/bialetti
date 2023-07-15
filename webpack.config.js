b865650e32be2064b1545f98773af41e4d64c0b4
Evan Taylor Yates <evantayloryates@gmail.com>
2023-07-15 16:11:05 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    // Where files should be sent once they are bundled
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js',
+    publicPath: process.env.NODE_ENV === 'production' ? '/bialetti/' : '/',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    },
+  },
+    // webpack 5 comes with devServer which loads in development mode
+  devServer: {
+    port: 4200,
+    watchContentBase: true,
+    historyApiFallback: true,
+  },
+  // Rules of how webpack will take our files, complie & bundle them for the browser 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      name: "index.html",
+      inject: false,
+      template: path.resolve(__dirname, './src/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+}
